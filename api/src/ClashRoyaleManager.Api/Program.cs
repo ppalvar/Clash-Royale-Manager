0263ab072dff333169ade99859f530901768be98
Pedro P. Alvarez Portelles <ppalvarez2002@gmail.com>
2024-02-02 11:30:59 -0500

    Initial commit

---
+++ api/src/ClashRoyaleManager.Api/Program.cs
@@ -0,0 +1,11 @@
+using FastEndpoints;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddFastEndpoints();
+
+var app = builder.Build();
+
+app.UseFastEndpoints();
+
+app.Run();
