425d56e65bb8bea8e5e2ed4238d9d4a5f26ccede
Abelito <abelitolld@gmail.com>
2024-03-05 20:36:58 -0500

    hacer login

---
+++ app/src/clash-royal/src/components/LoginVue.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="login-container">
+    <h2>Iniciar Sesión</h2>
+    <form class="login-form">
+      <input type="text" placeholder="Usuario" v-model="user" required /><br />
+      <input type="password" placeholder="Contraseña" v-model="password" required /><br />
+      <button type="submit" @click="authenticated">Entrar</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import consts from "../router/auth"
+
+export default {
+  data() {
+    return {
+      user: '',
+      password: '',
+    }
+  },
+
+  methods: {
+    authenticated() {
+      if (this.user != '' && this.password != '') {
+        consts.auth.value = true
+        this.$router.push("/")
+        this.user = this.password = ''
+      }
+    },
+  },
+}
+</script>
+
+<style>
+.login-container {
+  background-color: rgba(0, 0, 0, 0.75);
+  width: 250px;
+  margin: 50px auto;
+  padding: 20px;
+  border-radius: 15px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5);
+}
+
+.login-container h2 {
+  color: #ffde00;
+  text-shadow: 1px 1px 2px #000000;
+}
+
+.login-form {
+  display: flex;
+  flex-direction: column;
+}
+
+.login-form input {
+  margin-bottom: 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.25);
+}
+
+.login-form button {
+  background-color: #ffde00;
+  color: #121212;
+  padding: 10px;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+  font-weight: bold;
+  text-transform: uppercase;
+  transition: background-color 0.3s;
+}
+
+.login-form button:hover {
+  background-color: #f1c40f;
+}
+</style>
---
+++ app/src/clash-royal/src/views/LoginVue.vue
@@ -0,0 +1,16 @@
+<script>
+import Login from '@/components/LoginVue.vue'
+import Footer from '@/components/FooterDefault.vue'
+
+export default {
+  components: {
+    Login,
+    Footer,
+  },
+}
+</script>
+
+<template>
+    <Login />
+    <Footer />
+</template>
