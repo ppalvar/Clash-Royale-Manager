5605363b7819f285fb548388a7e97787be2b9b8a
Abelito <abelitolld@gmail.com>
2024-03-05 20:39:31 -0500

    crear tabla default

---
+++ app/src/clash-royal/src/components/SelectTypeEntity.vue
@@ -0,0 +1,38 @@
+<template>
+    <select class="search-entity" v-model="value" :onchange="$emit('search-entity', value)">
+        <option :value="player">Jugadores</option>
+        <option :value="cart">Cartas</option>
+        <option :value="clan">Clanes</option>
+    </select>
+</template>
+
+<script>
+import consts from '../router/auth'
+
+export default {
+    data() {
+        return {
+            player: consts.typeEntity.player,
+            cart: consts.typeEntity.cart,
+            clan: consts.typeEntity.clan,
+            value: '',
+        }
+    },
+
+    mounted(){
+        this.value = this.player
+    },
+}
+</script>
+
+<style>
+.search-entity {
+    display: flex;
+    padding: 8px;
+    widows: 15%;
+    border-radius: 8px;
+    position: absolute;
+    right: 10px;
+    top: 0px;
+}
+</style>
