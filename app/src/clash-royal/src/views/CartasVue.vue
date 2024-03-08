8ab24cf80d3ce3678e1bcb8c27a5ad87bb238a1b
Abelito <abelitolld@gmail.com>
2024-03-08 10:08:00 -0500

    agregar nuevas vistas de enlistar

---
+++ app/src/clash-royal/src/views/CartasVue.vue
@@ -0,0 +1,48 @@
+<script>
+import EntityDefaultViews from '../components/EntityDefaultViews.vue'
+import BBDD from '../router/BBDD'
+import consts from '../router/auth'
+import TableInfoCarta from '@/components/TableInfoCarta.vue'
+
+export default {
+    props: {
+        minimalice: {
+            type: Boolean,
+            default: false,
+        },
+    },
+
+    components: {
+        EntityDefaultViews,
+        TableInfoCarta,
+    },
+
+    data() {
+        return {
+            auth: consts.auth,
+        }
+    },
+
+    methods: {
+        seeInfo(id) {
+            this.$emit('info', id, consts.typeEntity.cart)
+        },
+
+        seeEntity() {
+            return BBDD.getAllCarts()
+        },
+    },
+}
+</script>
+
+<template>
+    <EntityDefaultViews>
+        <template #botonCrear>
+            <h2 v-if="auth && !minimalice">CrearCart</h2>
+        </template>
+
+        <template #tabla>
+            <TableInfoCarta :carts="seeEntity()" :minimalice="minimalice" @info="seeInfo" />
+        </template>
+    </EntityDefaultViews>
+</template>
---
+++ app/src/clash-royal/src/views/ClanesVue.vue
@@ -0,0 +1,48 @@
+<script>
+import EntityDefaultViews from '../components/EntityDefaultViews.vue'
+import BBDD from '../router/BBDD'
+import consts from '../router/auth'
+import TableInfoClan from '@/components/TableInfoClan.vue'
+
+export default {
+    props: {
+        minimalice: {
+            type: Boolean,
+            default: false,
+        },
+    },
+
+    components: {
+        EntityDefaultViews,
+        TableInfoClan,
+    },
+
+    data() {
+        return {
+            auth: consts.auth,
+        }
+    },
+
+    methods: {
+        seeInfo(id) {
+            this.$emit('info', id, consts.typeEntity.clan)
+        },
+
+        seeEntity() {
+            return BBDD.getAllClanes()
+        },
+    },
+}
+</script>
+
+<template>
+    <EntityDefaultViews>
+        <template #botonCrear>
+            <h2 v-if="auth && !minimalice">CrearClan</h2>
+        </template>
+
+        <template #tabla>
+            <TableInfoClan :clanes="seeEntity()" :minimalice="minimalice" @info="seeInfo" />
+        </template>
+    </EntityDefaultViews>
+</template>
